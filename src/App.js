d09abe8ff9cb29d6d5c519ae94bd4528b001fe19
Alexander Wilson <acwilson96@gmail.com>
2018-08-03 19:18:31 +0100

    Remove Debugging Code & Tidy Up Imports

--- src/App.js
+++ src/App.js
@@ -1,56 +1,46 @@
 import React, { Component } from 'react';
 
-import LandingPage from './Views/LoadingPage';
-import LoginPage from './Views/LoginPage';
-import AccountPage from './Views/AccountPage';
+import * as Views from './Views';
+
 import network from './Resources/networkHelper';
 
 class App extends Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        loading: true,
-        authenticated: false,
-        token: null
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      authenticated: false,
+      token: null
+    };
+  }
 
-    backdoor = (newToken) => {
-      alert("There is a Backdoor Open!");
+  /* On Component Mount, set state according to authToken status. */
+  componentDidMount = () => {
+    let localAuthToken = localStorage.getItem('authToken');
+
+    /* Check with backend if the token provides authentication. */
+    network.isAuthorised(localAuthToken, (authStatus) => {
+      // Set state appropriately.
       this.setState({
         loading: false,
-        authenticated: true,
-        token: newToken
+        authenticated: authStatus,
+        token: localAuthToken
       });
+    });
+  }
+
+  render = () => {
+    if (this.state.loading) {
+      return(<Views.Landing/>);
     }
-
-    /* On Component Mount, set state according to authToken status. */
-    componentDidMount = () => {
-      let localAuthToken = localStorage.getItem('authToken');
-
-      /* Check with backend if the token provides authentication. */
-      network.isAuthorised(localAuthToken, (authStatus) => {
-        // Set state appropriately.
-        this.setState({
-          loading: false,
-          authenticated: authStatus,
-          token: localAuthToken
-        });
-      });
+    else if (this.state.authenticated) {
+      return (<Views.Account/>);
     }
-
-    render = () => {
-      if (this.state.loading) {
-        return(<LandingPage/>);
-      }
-      else if (this.state.authenticated) {
-        return (<AccountPage/>);
-      }
-      else {
-        return (<LoginPage backdoor={this.backdoor} />);
-      }
-    } 
+    else {
+      return (<Views.Login />);
+    }
+  } 
 
 }
 
