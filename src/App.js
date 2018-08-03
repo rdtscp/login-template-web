214132f6f6a27872777ce37cdedf70ff07a498bb
Alexander Wilson <acwilson96@gmail.com>
2018-08-03 01:48:23 +0100

    App & Auth Checking

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+import LandingPage from './Views/LoadingPage';
+import LoginPage from './Views/LoginPage';
+import HomePage from './Views/HomePage';
+import network from './Resources/networkHelper';
+
+class App extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        loading: true,
+        authenticated: false,
+        token: null
+      };
+    }
+
+    backdoor = (newToken) => {
+      alert("There is a Backdoor Open!");
+      this.setState({
+        loading: false,
+        authenticated: true,
+        token: newToken
+      });
+    }
+
+    /* On Component Mount, set state according to authToken status. */
+    componentDidMount = () => {
+      let localAuthToken = localStorage.getItem('authToken');
+      console.log("localAuthToken: " + localAuthToken);
+
+      /* Check with backend if the token provides authentication. */
+      network.isAuthorised(localAuthToken, (authStatus) => {
+        console.log("Auth Status: " + authStatus);
+        // Set state appropriately.
+        this.setState({
+          loading: false,
+          authenticated: authStatus,
+          token: localAuthToken
+        });
+      });
+    }
+
+    render = () => {
+      if (this.state.loading) {
+        return(<LandingPage/>);
+      }
+      else if (this.state.authenticated) {
+        return (<HomePage/>);
+      }
+      else {
+        return (<LoginPage backdoor={this.backdoor} />);
+      }
+    } 
+
+}
+
+export default App;
