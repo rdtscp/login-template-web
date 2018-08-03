2a217086bd099c1093e432fe17ffe5b94ef6740b
Alexander Wilson <acwilson96@gmail.com>
2018-08-03 01:50:40 +0100

    User's Logged in HomePage

---
+++ src/Views/HomePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+  render = () => {
+    return (
+      <div className="landing-container">
+        <div style={{width: 300}}>
+          Home Page
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
