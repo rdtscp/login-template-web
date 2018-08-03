d7ae8cca35a90c7045449fd55515ef66430beaec
Alexander Wilson <acwilson96@gmail.com>
2018-08-03 01:50:45 +0100

    Loading Page

---
+++ src/Views/LoadingPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import { LoadingBar } from '../Components';
+
+class Landing extends Component {
+
+  render() {
+    return (
+      <div className="landing-page">
+        <LoadingBar />
+      </div>
+    );
+  }
+
+}
+
+export default Landing;
