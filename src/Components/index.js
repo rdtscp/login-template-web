c6a318d20226a3a068a85d690cef52d06cf0354a
Alexander Wilson <acwilson96@gmail.com>
2018-08-03 19:01:08 +0100

    Remove Redundant Imports

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,10 +1,8 @@
 import Button from './Button';
-import DeleteAccountPane from './SettingsPanes/DeleteAccountPane';
-import DevicesPane from './SettingsPanes/DevicesPane';
 import InputField from './InputField';
 import List from './List';
 import ListEntry from './ListEntry';
 import LoadingBar from './LoadingBar';
 
 
-export { Button, DeleteAccountPane, DevicesPane, InputField, List, ListEntry, LoadingBar };
+export { Button, InputField, List, ListEntry, LoadingBar };
