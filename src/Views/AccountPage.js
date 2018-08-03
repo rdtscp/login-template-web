9b4fc0fa99681ab87bb010e87efdc9be0a63b4d5
Alexander Wilson <acwilson96@gmail.com>
2018-08-03 17:26:54 +0100

    Account Page which Renders a Two-Pane Layout

---
+++ src/Views/AccountPage.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import * as Panes from '../Components/Panes';
+import { List } from '../Components';
+
+class AccountPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeListEntry: 'Devices',
+      listEntryMap: {
+        'Devices': <Panes.Devices />,
+        'Delete Account': <Panes.DeleteAccount />
+      }
+    };
+  }
+
+  activeListEntry = (entryName) => {
+    this.setState({
+      activeListEntry: entryName
+    });
+  }
+
+  render = () => {
+    return (
+      <div className="account-container">
+        <div className="settings-left-pane">
+          <List activateListEntry={this.activeListEntry} listEntryMap={this.state.listEntryMap} activeListEntry={this.state.activeListEntry} />
+        </div>
+        <div className="settings-right-pane">
+          {this.state.listEntryMap[this.state.activeListEntry]}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AccountPage;
