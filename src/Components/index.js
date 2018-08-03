ac054e926e946496622d422f5e8915331e91d8d7
Alexander Wilson <acwilson96@gmail.com>
2018-08-03 17:27:27 +0100

    Exports Components

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,10 @@
 import Button from './Button';
+import DeleteAccountPane from './SettingsPanes/DeleteAccountPane';
+import DevicesPane from './SettingsPanes/DevicesPane';
 import InputField from './InputField';
+import List from './List';
+import ListEntry from './ListEntry';
 import LoadingBar from './LoadingBar';
 
-export { Button, InputField, LoadingBar };
+
+export { Button, DeleteAccountPane, DevicesPane, InputField, List, ListEntry, LoadingBar };
