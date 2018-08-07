f031d0aa7ae5c8995ee3ce127c93b9793c247dea
Alexander Wilson <acwilson96@gmail.com>
2018-08-07 15:37:12 +0100

    Data Fetching & Passing to Children for Devices

--- src/Components/Panes/Devices.js
+++ src/Components/Panes/Devices.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Button, Device } from '../';
+import { Button, Device, LoadingSpinner } from '../';
 
 import axios from 'axios';
 
@@ -12,20 +12,29 @@ class Devices extends Component {
     super(props);
     this.state = {
       loading: true,
-      devicesList: [
-      ]
+      devicesList: [],
+      username: null
     }
   }
 
+  /* De-Authorises all devices under the users account. */
   logoutAll = () => {
     if(window.confirm("Are you sure you wish to log out all devices(including this one)?")) {
-      this.state.devicesList.forEach((device) => {
-        console.log("Logging Out Device: " + device.id);
-        this.logoutDevice(device);
-      });
+      this.state.devicesList.forEach((device) => this.logoutDevice(device));
     }
   }
 
+  /* Takes a Device to logout:
+   * {
+   *   createdAt:  this.props.createdAt,
+   *   id:         this.props.id,
+   *   authToken:  this.props.authToken,
+   *   ip:         this.props.ip,
+   *   userAgent:  this.props.userAgent,
+   * }
+   * 
+   * And De-Authorises the device from the users account.
+   */ 
   logoutDevice = (logoutDevice) => {
     let authToken = localStorage.getItem('authToken');
     network.getCSRF((csrfToken) => {
@@ -44,9 +53,9 @@ class Devices extends Component {
       .then((response) => {
         // If this responded properly.
         if (response.data) {
-          let res = response.data;
-          // If there were no errors or warning.
-          if (res.error === false && res.warning === false) {
+          let serverResponse = response.data;
+          // If there were no errors or warnings.
+          if (serverResponse.error === false && serverResponse.warning === false) {
             // Handle if we are logging this device out.
             if (logoutDevice.authToken === authToken) {
               alert("Logged Out of this Device");
@@ -54,22 +63,21 @@ class Devices extends Component {
             }
             else {
               alert("Device Logged Out");
-              let updatedDevices = this.state.devicesList.filter((device) => {
-                return device.id !== logoutDevice.id;
-              });
+              let updatedDevices = this.state.devicesList.filter((device) => device.id !== logoutDevice.id );
               this.setState({
                 devicesList: updatedDevices
               });
             }
           }
           else {
-            this.handleResponse(res);
+            this.handleResponse(serverResponse);
           }
         }
       });
     });
   }
  
+  /* On Component Mounting, Get the Devices this User is Authorised on. */
   componentDidMount = () => {
     let authToken = localStorage.getItem('authToken');
     network.getCSRF((csrfToken) => {
@@ -86,15 +94,17 @@ class Devices extends Component {
       .then((response) => {
         // If this responded properly.
         if (response.data) {
-          let res = response.data;
+          let serverResponse = response.data;
           // If there were no errors or warning.
-          if (res.error === false && res.warning === false) {
+          if (serverResponse.error === false && serverResponse.warning === false) {
             this.setState({
-              devicesList: response.data.content.user.devices
+              loading: false,
+              devicesList: serverResponse.content.user.devices,
+              username: serverResponse.content.user.username
             });
           }
           else {
-            this.handleResponse(res);
+            this.handleResponse(serverResponse);
           }
         }
       });
@@ -103,16 +113,30 @@ class Devices extends Component {
   
   render = () => {
     const devices = this.state.devicesList.map((deviceData) =>
-      <Device logout={this.logoutDevice} createdAt={deviceData.createdAt} id={deviceData.id} authToken={deviceData.authToken} ip={deviceData.ip} userAgent={deviceData.userAgent} key={deviceData.id} />
+      <Device logout={this.logoutDevice} lastUsed={deviceData.lastUsed} createdAt={deviceData.createdAt} id={deviceData.id} authToken={deviceData.authToken} ip={deviceData.ip} userAgent={deviceData.userAgent} key={deviceData.id} />
     );
-    return (
-      <div>
-        <Button title="Logout All" onClick={this.logoutAll} />
-        <div className="device-list-container">
-         {devices}
+    if (this.state.loading) {
+      return (
+        <div className="devices-loading-container">
+          <LoadingSpinner />
         </div>
-      </div>
-    );
+      );
+    }
+    else {
+      return (
+        <div style={{margin: 10, padding: 10,}}>
+          <div style={{float: 'left', fontSize: 25, paddingLeft: 10}}>
+            {this.state.username}
+          </div>
+          <div style={{float: 'right'}}>
+            <Button title="Logout All" onClick={this.logoutAll} />
+          </div>
+          <div style={{ marginTop: 50}}>
+          {devices}
+          </div>
+        </div>
+      );
+    }
   }
 
 }
