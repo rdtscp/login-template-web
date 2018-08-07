138d211ebab0c27f7df84a70a98c96e145cbc73c
Alexander Wilson <acwilson96@gmail.com>
2018-08-07 02:10:28 +0100

    Bugfix & Convert from UNIX to Timestamp

--- src/Resources/utilitiesHelper.js
+++ src/Resources/utilitiesHelper.js
@@ -2,13 +2,29 @@ const utilities = {
 
     /* @TODO: Make Nicer Popup */
     handleNetworkResponse: (res) => {
-    if (res.error) {
-      alert('Error: ' + res.mesasge);
+      if (res.error) {
+        alert('Error: ' + res.message);
+      }
+      else {
+        alert('Warning: ' + res.message);
+      }
+    },
+
+    unixToDateTime: (unixTimestamp) => {
+      let date          = new Date(unixTimestamp*1000);
+      let monthsStrings = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+      
+      let day     = date.getDate();
+      let month   = monthsStrings[date.getMonth()];
+      let year    = date.getFullYear();
+
+      let hours   = "0" + date.getHours();
+      let minutes = "0" + date.getMinutes();
+      var seconds = "0" + date.getSeconds();
+
+      let formattedTime = day + " " + month + " " + year + " at " + hours.substr(-2) + ":" + minutes.substr(-2) + ':' + seconds.substr(-2);
+      return formattedTime;
     }
-    else {
-      alert('Warning: ' + res.message);
-    }
-  }
 
 }
 
