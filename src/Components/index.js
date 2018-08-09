851d68cd0dc4087ce448ab54f9eed7cee2e38ca7
Alexander Wilson <acwilson96@gmail.com>
2018-08-09 02:19:01 +0100

    Accommodate new Components

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,10 +1,11 @@
 import Button from './Button';
 import Device from './Device';
+import Header from './Header';
 import InputField from './InputField';
-import List from './List';
-import ListEntry from './ListEntry';
+import LeftPane from './LeftPane';
+import LeftPaneEntry from './LeftPaneEntry';
 import LoadingBar from './LoadingBar';
 import LoadingSpinner from './LoadingSpinner';
 
 
-export { Button, Device, InputField, List, ListEntry, LoadingBar, LoadingSpinner };
+export { Button, Device, Header, InputField, LeftPane, LeftPaneEntry, LoadingBar, LoadingSpinner };
