ab01747481134f56ca9f10c18850d3407053d928
Alexander Wilson <acwilson96@gmail.com>
2018-08-09 10:00:18 +0100

    Create Popup Component

---
+++ src/Components/Popup/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Button } from '../';
+
+class Popup extends React.Component {
+    
+  render = () => {
+    return (
+      <div>
+        Modal/Popup        
+      </div>
+    );
+  }
+
+}
+
+export default Popup;
