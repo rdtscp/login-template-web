c8a2a7d259affedaf59fe5ddc2901a5c8bc7f6ff
Alexander Wilson <acwilson96@gmail.com>
2018-08-10 07:34:59 +0100

    Implementation for authToken checking and Logging In.

--- src/Actions/authActions.tsx
+++ src/Actions/authActions.tsx
@@ -1,39 +1,66 @@
 import axios                                          from 'axios';
 import network                                        from '../Resources/networkHelper';
 
-import { LOGIN_REQUEST }                              from './actionTypes';
+import { LOGIN_REQUEST, TOKEN_CHECK }                 from './actionTypes';
 
-const loginAction = (loginData: any) => ((dispatch: any) => {
-    network.getCSRF((csrfToken) => {
-      axios.request({
-        data: {
-          _csrf: csrfToken,
-          password: loginData.password,
-          username: loginData.username,
-        },
-        method: 'POST',
-        url: 'http://localhost:1337/device/create',
-        withCredentials: true,
-      })
-      .then((response) => {
-        // If this responded properly.
-        if (response.data.content.authToken) {
-          dispatch({
-            authToken: response.data.content.authToken,
-            authorised: true,
-          });
-        }
-        else {
-          dispatch({
-            actionType: LOGIN_REQUEST,
-            payload: {
-              authToken: '',
-              authorised: false,
-            },
-          });
-        }
-      });
-    });
+const checkAuthTokenAction = (authToken: string) => ((dispatch: any ) => {
+  // tslint:disable-next-line:no-console
+  console.log('Checking Auth Token');
+  network.isAuthorised(authToken, (authStatus: boolean) => {
+    dispatch({
+      payload: {
+        authStatus,
+        authToken,
+      },
+      type: TOKEN_CHECK
+    })
+  });
 });
 
-export { loginAction };
+const loginAction = (loginData: any) => ((dispatch: any) => {
+  network.getCSRF((csrfToken) => {
+    axios.request({
+      data: {
+        _csrf: csrfToken,
+        password: loginData.password,
+        username: loginData.username,
+      },
+      method: 'POST',
+      url: 'http://localhost:1337/device/create',
+      withCredentials: true,
+    })
+    .then((response) => {
+      // If this responded properly.
+      if (response.data.error || response.data.warning) {
+        dispatch({
+          payload: {
+            authStatus: false,
+            authToken: '',
+          },
+          type: LOGIN_REQUEST,
+        });
+      }
+      else if (response.data.content.authToken) {
+        localStorage.setItem('authToken', response.data.content.authToken);
+        dispatch({
+          payload: {
+            authStatus: true,
+            authToken: response.data.content.authToken,
+          },
+          type: LOGIN_REQUEST,
+        });
+      }
+      else {
+        dispatch({
+          payload: {
+            authStatus: false,
+            authToken: '',
+          },
+          type: LOGIN_REQUEST,
+        });
+      }
+    });
+  });
+});
+
+export { loginAction, checkAuthTokenAction };
