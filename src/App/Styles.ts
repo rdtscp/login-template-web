94b154681af577c15e4efb85d866ce999670dae6
Alexander Wilson <acwilson96@gmail.com>
2018-08-11 04:48:06 +0100

    Container for Loading Bar

--- src/App/Styles.ts
+++ src/App/Styles.ts
@@ -4,6 +4,15 @@ import { createStyles }                               from '@material-ui/core';
 import { WithStyles }                                 from '@material-ui/core/styles/withStyles';
 
 const appClasses = (theme: Theme) => createStyles({
+  loadingContainer: {
+    alignItems: 'center',
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center',
+    position: 'absolute',
+    width: '100%',
+
+  }
 });
 
 interface IAppStyle extends WithStyles<typeof appClasses> {};
