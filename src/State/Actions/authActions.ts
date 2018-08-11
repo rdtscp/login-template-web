67d0f068847ea46c1bc1c6af307614e7ec938dcf
Alexander Wilson <acwilson96@gmail.com>
2018-08-11 04:25:24 +0100

    AuthActions only has a setAuthState Action now.

--- src/State/Actions/authActions.ts
+++ src/State/Actions/authActions.ts
@@ -1,74 +1,16 @@
 import { Dispatch }                                   from 'redux';
 
-import axios                                          from 'axios';
-import network                                        from '../../Resources/networkHelper';
-
 /* Types */
-import { LOGIN_REQUEST, TOKEN_CHECK }                 from './actionTypes';
-import { ILoginRegisterInput }                        from './Types';
+import { SET_AUTH }                                   from './actionTypes';
 
-const checkAuthTokenAction = (authToken: string) => ((dispatch: Dispatch ) => {
-  network.isAuthorised(authToken, (authStatus: boolean) => {
-    dispatch({
-      payload: {
-        authStatus,
-        authToken,
-      },
-      type: TOKEN_CHECK
-    })
-  });
+const setAuthState = (authStatus: boolean, authToken: string) => ((dispatch: Dispatch) => {
+  dispatch({
+    payload: {
+      authStatus,
+      authToken,
+    },
+    type: SET_AUTH
+  })
 });
 
-
-
-const loginAction = (loginData: ILoginRegisterInput) => ((dispatch: Dispatch) => {
-  network.getCSRF((csrfToken) => {
-    axios.request({
-      data: {
-        _csrf: csrfToken,
-        password: loginData.password,
-        username: loginData.username,
-      },
-      method: 'POST',
-      url: 'http://localhost:1337/device/create',
-      withCredentials: true,
-    })
-    .then((response) => {
-      // If this responded properly.
-      if (response.data.error || response.data.warning) {
-        dispatch({
-          payload: {
-            authStatus: false,
-            authToken: '',
-          },
-          type: LOGIN_REQUEST,
-        });
-      }
-      else if (response.data.content.authToken) {
-        localStorage.setItem('authToken', response.data.content.authToken);
-        dispatch({
-          payload: {
-            authStatus: true,
-            authToken: response.data.content.authToken,
-          },
-          type: LOGIN_REQUEST,
-        });
-      }
-      else {
-        dispatch({
-          payload: {
-            authStatus: false,
-            authToken: '',
-          },
-          type: LOGIN_REQUEST,
-        });
-      }
-    });
-  });
-});
-
-// const registerAction = (registerData: ILoginRegisterInput) => ((dispatch: any) => {
-
-// });
-
-export { loginAction, checkAuthTokenAction };
+export { setAuthState };
