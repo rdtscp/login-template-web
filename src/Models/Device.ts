14787950cbe4148fc4c091db483bff4a7719f6d8
Alexander Wilson <acwilson96@gmail.com>
2018-08-11 12:48:31 +0100

    Device Model, with Types & API

---
+++ src/Models/Device.ts
@@ -0,0 +1,111 @@
+import axios from 'axios';
+import network from '../Resources/networkHelper';
+
+interface IDeviceType {
+  readonly createdAt:   number;
+  readonly updatedAt:   number;
+  readonly id:          string;
+  readonly lastUsed:    number;
+  readonly authToken:   string;
+  readonly ip:          string;
+  readonly userAgent:   string;
+  readonly owner:       string;
+}
+
+export type Device = IDeviceType;
+
+/* API Types */
+
+interface IBackendResponse {
+  data: IDeviceResponseData;
+}
+
+interface IDeviceResponseData {
+  error:    boolean;
+  warning:  boolean;
+  message:  string;
+  content:  { authStatus: boolean; authToken: string } |
+            { tokenValid: boolean } |
+            null;
+}
+
+export type DeviceResponseData = IDeviceResponseData;
+
+/* API */
+
+export const DeviceAPI = {
+
+  get(authToken: string) {
+    return new Promise((resolve, reject) => {
+      network.getCSRF((csrf: string) => {
+        axios.request({
+          data: {
+            _csrf:    csrf,
+            authToken
+          },
+          method: 'POST',
+          url: 'http://localhost:1337/device/get',
+          withCredentials: true,
+        })
+        .then((response: IBackendResponse) => {
+          const data: IDeviceResponseData = response.data;
+          return resolve(data);
+        })
+        .catch((error) => {
+          return reject(error);
+        })
+      });
+    });
+  },
+
+  create(authToken: string, username: string, password: string) {
+    return new Promise((resolve, reject) => {
+      network.getCSRF((csrf: string) => {
+        axios.request({
+          data: {
+            _csrf:    csrf,
+            authToken,
+            password,
+            username,
+          },
+          method: 'POST',
+          url: 'http://localhost:1337/device/create',
+          withCredentials: true,
+        })
+        .then((response: IBackendResponse) => {
+          const data: IDeviceResponseData = response.data;
+          return resolve(data);
+        })
+        .catch((error) => {
+          return reject(error);
+        })
+      });
+    });
+  },
+
+  destroy(authToken: string, deviceID: string, deviceAuthToken: string) {
+    return new Promise((resolve, reject) => {
+      network.getCSRF((csrf: string) => {
+        axios.request({
+          data: {
+            _csrf:    csrf,
+            authToken,
+            deviceAuthToken,
+            deviceID,
+          },
+          method: 'POST',
+          url: 'http://localhost:1337/device/destroy',
+          withCredentials: true,
+        })
+        .then((response: IBackendResponse) => {
+          const data: IDeviceResponseData = response.data;
+          return resolve(data);
+        })
+        .catch((error) => {
+          return reject(error);
+        })
+      });
+    });
+  },
+
+}
