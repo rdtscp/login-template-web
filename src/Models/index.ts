179afc044aeee8aca7f956d9c3e930a0d45f1037
Alexander Wilson <acwilson96@gmail.com>
2018-08-11 12:48:50 +0100

    Exports Models

---
+++ src/Models/index.ts
@@ -0,0 +1,13 @@
+import { Device, DeviceAPI, DeviceResponseData }  from './Device';
+import { User, UserAPI, UserReponseData }         from './User';
+
+
+
+export {
+  Device,
+  DeviceAPI,
+  DeviceResponseData,
+  User,
+  UserAPI,
+  UserReponseData
+};
