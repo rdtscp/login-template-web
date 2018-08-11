1c8da1fb5108775dd27b9e0c80059ddd1d58c59a
Alexander Wilson <acwilson96@gmail.com>
2018-08-11 04:22:14 +0100

    Tidy Up

--- src/Components/Device/index.ts
+++ src/Components/Device/index.ts
@@ -1,5 +1,9 @@
+/* Redux */
+
+/* Material-UI Styling */
 import withStyles                                     from '@material-ui/core/styles/withStyles';
 
+/* Component View */
 import Device                                         from './Device';
 import { deviceClasses }                              from './Styles';
 
