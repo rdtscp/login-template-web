5493ff66dc65e29abd70d14908e5e196b389b06e
Alexander Wilson <acwilson96@gmail.com>
2018-08-12 19:56:12 +0100

    More responsive design

--- src/Components/DeviceList/Styles.ts
+++ src/Components/DeviceList/Styles.ts
@@ -4,6 +4,13 @@ import { createStyles }         from '@material-ui/core';
 import { WithStyles }           from '@material-ui/core/styles/withStyles';
 
 const deviceListClasses = (theme: Theme) => createStyles({
+  devicesContainer: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 10,
+    maxWidth: 355,
+    minWidth: 327,
+  },
 });
 
 interface IDeviceListStyle extends WithStyles<typeof deviceListClasses> {};
