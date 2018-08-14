27b7d675c56993140801697115ee121a8f777329
Alexander Wilson <acwilson96@gmail.com>
2018-08-14 20:42:23 +0100

    Remove Test

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 /* ReactJS */
-import axios                                          from 'axios';
 import * as React                                     from 'react';
 import * as ReactDOM                                  from 'react-dom';
 import registerServiceWorker                          from './registerServiceWorker';
@@ -12,8 +11,6 @@ import store                                          from './State/Store';
 
 /* Styles */
 import './styles.css';
-
-axios.get('https://login-template-core.herokuapp.com/device/create');
 
 ReactDOM.render(
   <Provider store={store}>
