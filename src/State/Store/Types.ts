c39a622a2b72c83793d3a86bdad9bb5ac728470e
Alexander Wilson <acwilson96@gmail.com>
2018-08-15 16:36:38 +0100

    Bugfix

--- src/State/Store/Types.ts
+++ src/State/Store/Types.ts
@@ -3,13 +3,13 @@ import * as Models                                    from 'src/Models';
 /* Overall State */
 
 export interface IStateType {
-  readonly authState:   IIAuthStateType;
+  readonly authState:   IAuthStateType;
   readonly currentUser: Models.User;
 }
 
 /* Non Model Types */
 
-export interface IIAuthStateType {
+export interface IAuthStateType {
   readonly authStatus: boolean;
   readonly authToken:  string;
 }
